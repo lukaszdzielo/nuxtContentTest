5e98c05b4476316696b9e23e03772509f4e09b84
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-10-05 18:33:17 +0200

    test nuxt

---
+++ app/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <div>1</div>
+      <img src="" alt="Logo" >
+    </div>
+    <div>
+      <div>2</div>
+      <NuxtImg src="/nuxt-icon.png" />
+    </div>
+    <div>
+      <div>3</div>
+      <NuxtPicture format="webp" src="/nuxt-icon.png" />
+    </div>
+
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
