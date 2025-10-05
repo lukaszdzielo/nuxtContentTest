8dcc3bce5b643be1c134310dda21e0b862f36302
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-10-05 18:51:03 +0200

    img path

--- app/app.vue
+++ app/app.vue
@@ -2,18 +2,25 @@
   <div>
     <div>
       <div>1</div>
-      <img src="/public/pexels-eberhardgross-1062175.jpg" alt="Logo" >
+      <img src="..//public/pexels-eberhardgross-1062175.jpg" alt="Logo" >
     </div>
     <div>
       <div>2</div>
-      <NuxtImg src="/public/pexels-eberhardgross-1062175.jpg" />
+      <NuxtImg src="../public/pexels-eberhardgross-1062175.jpg" />
     </div>
     <div>
       <div>3</div>
-      <NuxtPicture format="webp" src="/public/pexels-eberhardgross-1062175.jpg" />
+      <NuxtPicture format="webp" src="../public/pexels-eberhardgross-1062175.jpg" />
     </div>
+    
 
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
   </div>
 </template>
+
+<style>
+  img {
+    max-width: 100%;
+  }
+</style>
