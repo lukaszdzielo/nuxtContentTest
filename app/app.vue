9974c30a2255a16f78abd535c438342fec9958b5
Lukasz Dev <lukaszdzielo.dev@gmail.com>
2025-10-05 18:51:58 +0200

    img

--- app/app.vue
+++ app/app.vue
@@ -2,15 +2,15 @@
   <div>
     <div>
       <div>1</div>
-      <img src="..//public/pexels-eberhardgross-1062175.jpg" alt="Logo" >
+      <img src="../public/pexels-eberhardgross-1062175.jpg" alt="img1" >
     </div>
     <div>
       <div>2</div>
-      <NuxtImg src="../public/pexels-eberhardgross-1062175.jpg" />
+      <NuxtImg src="../public/pexels-eberhardgross-1062175.jpg" alt="img2" />
     </div>
     <div>
       <div>3</div>
-      <NuxtPicture format="webp" src="../public/pexels-eberhardgross-1062175.jpg" />
+      <NuxtPicture format="webp" src="../public/pexels-eberhardgross-1062175.jpg" alt="img3" />
     </div>
     
 
